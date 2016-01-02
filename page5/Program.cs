71d8b3ba11db29e2a455a73bef722644247c92ac
Tom Owsiak <towsiak@yahoo.com>
2016-01-01 22:15:34 -0500

    page 5

---
+++ page5/Program.cs
@@ -0,0 +1,8 @@
+﻿using System; //1) Needed to use external features
+class Program //2) Outermost level of grouping
+{
+    static void Main() //3) Loads into memory first
+    { //4) Enters Main
+        Console.WriteLine("First Program!"); //5)Shows First Program!
+    } //6) Exits Main
+}
